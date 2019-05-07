d50ffd5b7413d2768595befe8d674e46805bbf4d
yjw <m13915528694_1@163.com>
2019-05-07 18:12:58 +0800

    添加了add 方法

--- app/src/main/java/hd/demogithub/MainActivity.java
+++ app/src/main/java/hd/demogithub/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public int add(int a,int b){
+
+        return a + b;
+    }
 }
